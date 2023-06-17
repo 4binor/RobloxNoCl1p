a47e4dcd688c9462bd29d4dd8238da404034beb2
4binor <96974108+4binor@users.noreply.github.com>
2023-06-17 13:35:31 +0600

    main.lua

--- main.lua
+++ main.lua
@@ -1,175 +1,47 @@
-local Workspace = game:GetService("Workspace")
-local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 
-local Noclip = Instance.new("ScreenGui")
-Noclip.Name = "Noclip"
-Noclip.Parent = CoreGui
-
-local BG = Instance.new("Frame")
-BG.Name = "BG"
-BG.Parent = Noclip
-BG.BackgroundColor3 = Color3.new(0, 0, 0)
-BG.BorderColor3 = Color3.new(0.0588235, 0.0588235, 0.0588235)
-BG.BorderSizePixel = 2
-BG.Position = UDim2.new(0.149479166, 0, 0.82087779, 0)
-BG.Size = UDim2.new(0, 280, 0, 230)
-BG.Active = true
-BG.Draggable = true
-
-local Title = Instance.new("TextLabel")
-Title.Name = "Title"
-Title.Parent = BG
-Title.BackgroundColor3 = Color3.new(1, 0.843137, 0)
-Title.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-Title.BorderSizePixel = 2
-Title.Size = UDim2.new(0, 280, 0, 33)
-Title.Font = Enum.Font.Highway
-Title.Text = "Noclip"
-Title.TextColor3 = Color3.new(1, 1, 1)
-Title.FontSize = Enum.FontSize.Size32
-Title.TextSize = 30
-Title.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Title.TextStrokeTransparency = 0
-
-local Toggle = Instance.new("TextButton")
-Toggle.Parent = BG
-Toggle.BackgroundColor3 = Color3.new(1, 0.843137, 0)
-Toggle.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-Toggle.BorderSizePixel = 2
-Toggle.Position = UDim2.new(0.152380958, 0, 0.109565221, 0)
-Toggle.Size = UDim2.new(0, 120, 0, 36)
-Toggle.Font = Enum.Font.Highway
-Toggle.FontSize = Enum.FontSize.Size28
-Toggle.Text = "Toggle"
-Toggle.TextColor3 = Color3.new(1, 1, 1)
-Toggle.TextSize = 25
-Toggle.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Toggle.TextStrokeTransparency = 0
-
-local StatusPF = Instance.new("TextLabel")
-StatusPF.Name = "StatusPF"
-StatusPF.Parent = BG
-StatusPF.BackgroundColor3 = Color3.new(1, 1, 1)
-StatusPF.BackgroundTransparency = 1
-StatusPF.Position = UDim2.new(0.314285725, 0, 0.255072743, 0)
-StatusPF.Size = UDim2.new(0, 80, 0, 20)
-StatusPF.Font = Enum.Font.Highway
-StatusPF.FontSize = Enum.FontSize.Size24
-StatusPF.Text = "Status:"
-StatusPF.TextColor3 = Color3.new(1, 1, 1)
-StatusPF.TextSize = 20
-StatusPF.TextStrokeColor3 = Color3.new(0.333333, 0.333333, 0.333333)
-StatusPF.TextStrokeTransparency = 0
-StatusPF.TextWrapped = true
-
-local Status = Instance.new("TextLabel")
-Status.Name = "Status"
-Status.Parent = BG
-Status.BackgroundColor3 = Color3.new(1, 1, 1)
-Status.BackgroundTransparency = 1
-Status.Position = UDim2.new(0.550000012, 0, 0.255072743, 0)
-Status.Size = UDim2.new(0, 60, 0, 20)
-Status.Font = Enum.Font.Highway
-Status.FontSize = Enum.FontSize.Size14
-Status.Text = "off"
-Status.TextColor3 = Color3.new(0.666667, 0, 0)
-Status.TextScaled = true
-Status.TextSize = 14
-Status.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Status.TextWrapped = true
-Status.TextXAlignment = Enum.TextXAlignment.Left
-
-local Credit = Instance.new("TextLabel")
-Credit.Name = "Credit"
-Credit.Parent = BG
-Credit.BackgroundColor3 = Color3.new(1, 1, 1)
-Credit.BackgroundTransparency = 1
-Credit.Position = UDim2.new(0.114285723, 0, 0.860240936, 0)
-Credit.Size = UDim2.new(0, 200, 0, 17)
-Credit.Font = Enum.Font.SourceSans
-Credit.FontSize = Enum.FontSize.Size18
-Credit.Text = "Created by KingLuna\nVersion 1.0.0"
-Credit.TextColor3 = Color3.new(1, 1, 1)
-Credit.TextSize = 16
-Credit.TextStrokeColor3 = Color3.new(0.196078, 0.196078, 0.196078)
-Credit.TextStrokeTransparency = 0
-Credit.TextWrapped = true
-
-local Fly = Instance.new("TextButton")
-Fly.Name = "Fly"
-Fly.Parent = BG
-Fly.BackgroundColor3 = Color3.new(1, 0.843137, 0)
-Fly.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-Fly.BorderSizePixel = 2
-Fly.Position = UDim2.new(0.152380958, 0, 0.273912132, 0)
-Fly.Size = UDim2.new(0, 120, 0, 36)
-Fly.Font = Enum.Font.Highway
-Fly.FontSize = Enum.FontSize.Size28
-Fly.Text = "Fly"
-Fly.TextColor3 = Color3.new(1, 1, 1)
-Fly.TextSize = 25
-Fly.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Fly.TextStrokeTransparency = 0
-
 local Plr = Players.LocalPlayer
-local Clipon = false
 local FlyEnabled = false
 
-Toggle.MouseButton1Click:Connect(function()
-    if Status.Text == "off" then
-        Clipon = true
-        Status.Text = "on"
-        Status.TextColor3 = Color3.new(0, 185, 0)
-    else
-        Clipon = false
-        Status.Text = "off"
-        Status.TextColor3 = Color3.new(170, 0, 0)
-    end
-end)
+local function Fly()
+    local character = Plr.Character or Plr.CharacterAdded:Wait()
+    local humanoid = character:WaitForChild("Humanoid")
 
-Fly.MouseButton1Click:Connect(function()
-    FlyEnabled = not FlyEnabled
-    if FlyEnabled then
-        Plr.CharacterAdded:Connect(function(character)
-            character:WaitForChild("Humanoid").Died:Connect(function()
-                FlyEnabled = false
-            end)
-        end)
-    end
-end)
+    humanoid.PlatformStand = true
+    humanoid.Sit = true
+    humanoid.JumpPower = 0
 
-local function Clip()
-    if Clipon then
-        for _, child in ipairs(Workspace:GetDescendants()) do
-            if child:IsA("BasePart") and child.CanCollide == true then
-                child.CanCollide = false
-            end
+    local flyVector = Vector3.new(0, 0, 0)
+    local speed = 50
+
+    local function updateFlyVector()
+        local forward = (UserInputService:IsKeyDown(Enum.KeyCode.W) and 1 or 0) - (UserInputService:IsKeyDown(Enum.KeyCode.S) and 1 or 0)
+        local right = (UserInputService:IsKeyDown(Enum.KeyCode.D) and 1 or 0) - (UserInputService:IsKeyDown(Enum.KeyCode.A) and 1 or 0)
+
+        flyVector = Vector3.new(right, forward, 0).unit * speed
+    end
+
+    UserInputService.InputBegan:Connect(function(input)
+        if input.KeyCode == Enum.KeyCode.Space then
+            FlyEnabled = not FlyEnabled
+            humanoid.PlatformStand = FlyEnabled
+            updateFlyVector()
         end
-    end
-end
+    end)
 
-local function Unclip()
-    for _, child in ipairs(Workspace:GetDescendants()) do
-        if child:IsA("BasePart") and child.CanCollide == false then
-            child.CanCollide = true
+    UserInputService.InputChanged:Connect(function(input)
+        if input.KeyCode == Enum.KeyCode.W or input.KeyCode == Enum.KeyCode.A or
+            input.KeyCode == Enum.KeyCode.S or input.KeyCode == Enum.KeyCode.D then
+            updateFlyVector()
         end
-    end
-end
+    end)
 
-UserInputService.InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode.Space then
+    game:GetService("RunService").Heartbeat:Connect(function()
         if FlyEnabled then
-            Plr.Character:FindFirstChildOfClass("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
+            character.HumanoidRootPart.Velocity = flyVector
         end
-    end
-end)
+    end)
+end
 
-game:GetService("RunService").RenderStepped:Connect(function()
-    if FlyEnabled then
-        Plr.Character:FindFirstChildOfClass("Humanoid"):ChangeState(Enum.HumanoidStateType.Flying)
-    end
-
-    Clip()
-end)
+Fly()
